1e3ca9733ce2f290b3f24949faa976488e102e5f
name2547name287-crypto <name2547name287@gmail.com>
2025-12-28 22:35:26 +0000

    0.4.1.5

--- js/auth.js
+++ js/auth.js
@@ -8,3 +8,29 @@ function login(){
     .then(()=>alert("Login สำเร็จ"))
     .catch(err=>alert(err.message));
 }
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+async function register() {
+  const email = emailEl.value;
+  const pass = passwordEl.value;
+  const username = usernameEl.value;
+
+  const res = await auth.createUserWithEmailAndPassword(email, pass);
+
+  await db.collection("users").doc(res.user.uid).set({
+    username,
+    role: "customer"
+  });
+
+  location.href = "index.html";
+}
+
+async function login() {
+  await auth.signInWithEmailAndPassword(
+    emailEl.value,
+    passwordEl.value
+  );
+  location.href = "index.html";
+}
